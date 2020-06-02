f47f2bd13d1022c9b2cf674676c59e8e10cb2465
a.elyuta <a.elyuta@robo.finance>
2020-06-02 14:06:35 +0700

    Fixed type of dueDate column

--- src/main/resources/db/migration/V1.0__create_todo_table.sql
+++ src/main/resources/db/migration/V1.0__create_todo_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE todo (
     title VARCHAR(255) NOT NULL,
     description VARCHAR(500) NOT NULL,
     created_at DATE NOT NULL,
-    due_date DATE NOT NULL,
+    due_date DATETIME NOT NULL,
     done BOOLEAN NOT NULL,
 
     CONSTRAINT pk_todo PRIMARY KEY (id)
